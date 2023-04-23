167201f81136b8737708534ba2c10d45a8bccb7f
Luis <carcamocariasl@yahoo.com>
2023-04-22 21:04:23 -0600

    configurando archivo vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base:'https://LuisCC21.github.io/Peliculas-App/'
+  base: 'https://LuisCC21.github.io/Peliculas-App/',
 })
